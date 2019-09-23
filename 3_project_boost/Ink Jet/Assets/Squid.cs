e527eba4d8df083b31ebc104f45a011c0d8b47d3
crazyscarecrow <42651599+crazyscarecrow@users.noreply.github.com>
2019-09-23 12:09:58 -0600

    first 2 levels

--- 3_project_boost/Ink Jet/Assets/Squid.cs
+++ 3_project_boost/Ink Jet/Assets/Squid.cs
@@ -1,11 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Squid : MonoBehaviour {
 
+    [SerializeField] float rotationThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float ascensionTime = 1f;
+    [SerializeField] float deathTime = 1f;
+    [SerializeField] AudioClip thrust;
+    [SerializeField] AudioClip win;
+    [SerializeField] AudioClip death;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { ALIVE, DYING, ASCENDING}
+
+    State state = State.ALIVE;
 
     // Use this for initialization
     void Start()
@@ -17,32 +31,75 @@ public class Squid : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.ALIVE)
+        {
+            RespondToThrustInput();
+            RespondToRotationInput();
+        }
+        
     }
 
-    private void Rotate()
+    private void OnCollisionEnter(Collision collision)
+    {
+        //ignore collisions when dead
+        if(state != State.ALIVE){return;}
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                {
+                    break;
+                }
+            case "Finish":
+                {
+                    state = State.ASCENDING;
+                    audioSource.Stop();
+                    audioSource.PlayOneShot(win);
+                    Invoke("LoadNextLevel", ascensionTime);
+                    break;
+                }
+            default:
+                {
+                    state = State.DYING;
+                    audioSource.Stop();
+                    audioSource.PlayOneShot(death);
+                    Invoke("LoadFirstLevel", deathTime);
+                    break;
+                }
+
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void RespondToRotationInput()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            rigidBody.AddRelativeTorque(Vector3.forward, ForceMode.Force);
+            rigidBody.AddRelativeTorque(Vector3.forward * rotationThrust, ForceMode.Force);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            rigidBody.AddRelativeTorque(Vector3.back, ForceMode.Force);
+            rigidBody.AddRelativeTorque(Vector3.back * rotationThrust, ForceMode.Force);
         }
     }
 
-    private void Thrust()
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.W))
         {
             if (!audioSource.isPlaying)
             {
-                audioSource.Play();
+                audioSource.PlayOneShot(thrust);
             }
-            rigidBody.AddRelativeForce(Vector3.up);
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
         }
         else
         {
